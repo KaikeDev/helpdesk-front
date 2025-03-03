b171be50c7459b497e5c115a4e98b47ca28fa1e3
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-03-03 12:41:05 -0300

    Finalizando o CSS e HTML do componente tecnico-list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Tecnico } from '../../../models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   standalone: true,
-  imports: [],
+  imports: [MatPaginator, MatTableModule],
   templateUrl: './tecnico-list.component.html',
   styleUrl: './tecnico-list.component.scss'
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent implements AfterViewInit{
+
+  ELEMENT_DATA:Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Kaike Tuerpe',
+      cpf: '112.030.345-98',
+      email: 'kaike@gmail.com',
+      senha: '1234',
+      perfil: ['0'],
+      dataCriacao: '03/03/2025'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
