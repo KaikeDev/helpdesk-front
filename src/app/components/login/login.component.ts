98a2288465c5feb9e346e4922551de522c8c3cc0
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-03-04 11:04:33 -0300

    Validação de login criada

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { Credenciais } from '../../models/credenciais';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormField, MatLabel],
+  imports: [
+    MatFormField,
+    MatLabel,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  creds: Credenciais = {
+    email: '',
+    senha: '',
+  };
 
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  validaCampos(): boolean {
+    if (this.email.valid && this.senha.valid) {
+      return true;
+    }
+    return false;
+  }
 }
