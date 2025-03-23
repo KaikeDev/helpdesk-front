6fc982e273d912501f8bc5554e68e7621cbb6d71
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-03-23 16:13:13 -0300

    Criando o formulário para cadastrar um novo técnico

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { FormsModule,  } from '@angular/forms';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-tecnico-create',
   standalone: true,
-  imports: [],
+  imports: [MatCheckbox, FormsModule, MatFormField, MatLabel, MatIcon, FormsModule, MatInputModule],
   templateUrl: './tecnico-create.component.html',
   styleUrl: './tecnico-create.component.scss'
 })
