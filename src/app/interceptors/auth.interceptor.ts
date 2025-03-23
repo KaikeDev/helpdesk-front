c38e36df933386ba2f9cf3acc6c1b6065914da82
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-03-23 14:33:00 -0300

    Adicionando o interceptor na aplicação

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,24 +1,22 @@
-
-import { HTTP_INTERCEPTORS, HttpHandler, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { inject } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  let token = localStorage.getItem('token')
-  let proximo = inject(HttpHandler)
+  let token = localStorage.getItem('token');
 
-  if(token ){
-
-    const cloneReq = req.clone({headers: req.headers.set('Authorization', `Bearer ${token}`)}) // para pegar o token no header do authorization do backend
-    return proximo.handle(cloneReq)
-  }else{
-    return proximo.handle(req)
+  if (token) {
+    const cloneReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    return next(cloneReq);
   }
+
+  return next(req);
 };
 
-export const  AuthInterceptorProvider = [
+export const AuthInterceptorProvider = [
   {
     provide: HTTP_INTERCEPTORS,
-    useClass: authInterceptor,
+    useValue: authInterceptor, // ✅ Functional interceptors use `useValue`
     multi: true
   }
-]
+];
