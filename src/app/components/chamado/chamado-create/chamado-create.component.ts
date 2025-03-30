f219d5d6870e9b67eb2fee7c7a2f764e72f6edb5
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-03-30 16:08:52 -0300

    Primeira parte da tela de criar um chamado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatOption } from '@angular/material/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { Chamado } from '../../../models/chamado';
+import { Cliente } from '../../../models/cliente';
+import { Tecnico } from '../../../models/tecnico';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-chamado-create',
   standalone: true,
-  imports: [],
+  imports: [MatFormField, MatLabel, FormsModule, ReactiveFormsModule, MatOption, MatSelect, MatSelectModule, MatInputModule],
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.scss'
 })
 export class ChamadoCreateComponent {
+
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status:     FormControl = new FormControl(null, [Validators.required]);
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  observacoes:FormControl = new FormControl(null, [Validators.required]);
+  tecnico:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
 
 }
